5f4ae170341c6fce42a6c244586fbbb3fd3f4415
23birola <23birola@gmail.com>
2024-02-20 16:01:09 +0000

    edit styles

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,14 +7,15 @@ export default function WorkCard({ data }) {
     backgroundPosition: 'center',
   };
   return (
-    <Link to={data.url} className='project'>
+    <Link to={data.pageUrl} className='project'>
       <div className="card">
        <img src={data.img} alt={data.name} className='card-img' />
-              <div className="card-body work-info">
-                <h5 className="card-title">{data.name}</h5>
-                <p className="card-text">{data.skills.join(', ')}</p>
-              </div>
-            </div>
+          <div className="card-body work-info">
+            <h5 className="card-title">{data.name}</h5>
+            <p className="card-text">{data.skills.join(', ')}</p>
+          <Link to={data.url} className='project-github'>GitHub Link</Link>
+          </div>
+      </div>
     </Link>
     
   );
