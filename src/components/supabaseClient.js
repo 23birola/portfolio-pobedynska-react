0416e12ac4a8e0953f00d6f345fc4d77557df6ab
23birola <23birola@gmail.com>
2025-01-04 16:52:00 +0000

    initial file

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tadcgmzjdxjzflrszhhi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhZGNnbXpqZHhqemZscnN6aGhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5NDI4OTUsImV4cCI6MjA1MTUxODg5NX0.UyJkUAUD9uI7XqOAQItpp8kf6M06FMgbZ7htAhooSPM';
+const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
